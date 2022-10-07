184c781735a12d8793290d5aaa92694c9725de24
missopi <missopi@me.com>
2022-10-07 21:18:48 +0100

    Determine number and colour combination of pegs

--- main.rb
+++ main.rb
@@ -2,3 +2,12 @@
 
 require 'colorize'
 
+puts ' '
+puts "================== MASTERMIND =================="
+puts ' '
+
+peg = ['  1  '.colorize(:color => :black, :background => :light_red), '  2  '.colorize(:background => :yellow), 
+    '  3  '.colorize(:color => :black, :background => :light_blue), '  4  '.colorize(:background => :green), 
+    '  5  '.colorize(:color => :black, :background => :light_magenta), '  6  '.colorize(:background => :cyan)]
+
+puts "#{peg[0]}" + " " + "#{peg[1]}" + " " + "#{peg[2]}" + " " + "#{peg[3]}" + " " + "#{peg[4]}" + " " + "#{peg[5]}"
