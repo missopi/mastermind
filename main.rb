862d3eb78707bef46ad5144671f2eab6835c62dd
missopi <missopi@me.com>
2022-10-07 22:44:39 +0100

    Add code to game class

--- main.rb
+++ main.rb
@@ -6,6 +6,12 @@ puts ' '
 puts "================== MASTERMIND =================="
 puts ' '
 
+class HumanPlayer
+end
+
+class ComputerPlayer
+end
+
 class Pegs
     def initialize 
         code_pegs = ['  1  '.colorize(:color => :black, :background => :light_red), '  2  '.colorize(:background => :yellow), 
@@ -14,34 +20,39 @@ class Pegs
         
         key_pegs = [' ● '.colorize(:color => :white), ' ● '.colorize(:color => :red) ]
 
-        puts "#{code_pegs[0]}" + " " + "#{code_pegs[1]}" + " " + "#{code_pegs[2]}" + " " + "#{code_pegs[3]}" + " " + "#{code_pegs[4]}" + " " + "#{code_pegs[5]}" + '    ' + "#{key_pegs[0]}" + "#{key_pegs[1]}"
+        puts "#{code_pegs[0]}" + " " + "#{code_pegs[1]}" + " " + "#{code_pegs[2]}" + " " + "#{code_pegs[3]}" + " " + "#{code_pegs[4]}" + " " + "#{code_pegs[5]}" 
+        puts ' '
+        puts "#{key_pegs[0]}" + "#{key_pegs[1]}"
         puts ' '
 
     end
     
-    def display_code
-    end
-
     def update_code
     end       
 end
 
-Pegs.new
-
-class HumanPlayer
-end
-
-class ComputerPlayer
-end
-
 class Game
+    def initialize
+        @pegs = Pegs.new
+    end
+
+    def convert_choice_to_pegs
+        puts 'Enter a 4 digit code to guess the secret code'
+        choice = gets.split('').to_a 
+        choice.pop # removes carriage return rom array
+    end
+
+    def display_pegs
+        puts "#{code_pegs[choice[0]]}" + " " + "#{code_pegs[choice[1]]}" + " " + "#{code_pegs[choice[2]]}" + " " + "#{code_pegs[choice[3]]}"
+    end
 end
 
+Game.new
 
-puts 'Enter a 4 digit code to guess the secret code'
-choice = gets.split('').to_a 
-choice.pop # removes carriage return rom array
 
-puts "Turn number #{}" + "#{choice[0]}" + " " + "#{choice[1]}" + " " + "#{choice[2]}" + " " + "#{choice[3]}"
+
+
+
+
 
 
