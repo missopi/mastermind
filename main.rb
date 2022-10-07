3f12bdf4eaac9fa92e7e7c36427c53809c52c3d3
missopi <missopi@me.com>
2022-10-07 21:44:03 +0100

    Display player guesses of code

--- main.rb
+++ main.rb
@@ -11,3 +11,13 @@ peg = ['  1  '.colorize(:color => :black, :background => :light_red), '  2  '.co
     '  5  '.colorize(:color => :black, :background => :light_magenta), '  6  '.colorize(:background => :cyan)]
 
 puts "#{peg[0]}" + " " + "#{peg[1]}" + " " + "#{peg[2]}" + " " + "#{peg[3]}" + " " + "#{peg[4]}" + " " + "#{peg[5]}"
+
+
+
+puts 'Enter a 4 digit code to guess the secret code'
+choice = gets.split('').to_a 
+choice.pop # removes carriage return rom array
+
+puts "Turn number #{}" + "#{choice[0]}" + " " + "#{choice[1]}" + " " + "#{choice[2]}" + " " + "#{choice[3]}"
+
+
