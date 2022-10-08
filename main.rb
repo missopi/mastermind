09d2c10b5dc115e2447b792f6f08b4b5610941fb
missopi <missopi@me.com>
2022-10-08 10:55:44 +0100

    Seperate different pegs into their own classes

--- main.rb
+++ main.rb
@@ -12,24 +12,37 @@ end
 class ComputerPlayer
 end
 
-# class to assign and display the coloured code of the pegs and the keys to show when guesses are correct
-class Pegs
-  attr_reader :code_pegs, :key_pegs
+# handles the coloured keys that show when guesses are correct
+class KeyPegs
+  attr_reader :key_pegs
 
-  def initialize 
-    @code_pegs = ['  1  '.colorize(color: black, background: light_red), '  2  '.colorize(background: yellow),
-                  '  3  '.colorize(color: black, background: light_blue), '  4  '.colorize(background: green),
-                  '  5  '.colorize(color: black, background: light_magenta), '  6  '.colorize(background: cyan)]
+  def initialize
     @key_pegs = [' ● '.colorize(color: white), ' ● '.colorize(color: red)]
+  end
 
-    puts code_pegs[0].to_s + ' ' + code_pegs[1].to_s + ' ' + code_pegs[2].to_s + ' ' + code_pegs[3].to_s + ' ' + code_pegs[4].to_s + ' ' + code_pegs[5].to_s 
+  def initial_keys(key_pegs)
     puts ' '
-    puts key_pegs[0].to_s + key_pegs[1].to_s
+    puts key_pegs[1].to_s + key_pegs[1].to_s + key_pegs[0].to_s + key_pegs[0].to_s
     puts ' '
   end
 
-  def update_pegs
+  def display_keys(key_pegs) end
+end
 
+# handles the coloured pegs that show the code in the terminal
+class CodePegs
+  attr_reader :code_pegs
+
+  def initialize
+    @code_pegs = ['  1  '.colorize(color: black, background: light_red), '  2  '.colorize(background: yellow),
+                  '  3  '.colorize(color: black, background: light_blue), '  4  '.colorize(background: green),
+                  '  5  '.colorize(color: black, background: light_magenta), '  6  '.colorize(background: cyan)]
+  end
+
+  def initial_pegs(code_pegs)
+    puts code_pegs[0].to_s + ' ' + code_pegs[1].to_s + ' ' + code_pegs[2].to_s + ' ' + code_pegs[3].to_s + ' ' + code_pegs[4].to_s + ' ' + code_pegs[5].to_s 
+    puts ' '
+  end
 end
 
 # class to actually play the game
@@ -51,6 +64,3 @@ class Game
     "#{@code_pegs[choice[2]]}" + ' ' + "#{@code_pegs[choice[3]]}"
   end
 end
-
-mastermind = Game.new
-mastermind.display_pegs
