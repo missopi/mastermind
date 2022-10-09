e19f46f11a50afe47ce1ea98b8707b6a41f67ffa
missopi <missopi@me.com>
2022-10-09 22:08:46 +0100

    Move human game to its own file and have main game

--- main.rb
+++ main.rb
@@ -1,60 +1,27 @@
 # frozen_string_literal: true
 
 require 'colorize'
-require_relative 'rules'
+require_relative 'human_game'
+require_relative 'comp_game'
 
-# handles playing the actual game of mastermind if human playing
-class HumanGame
+# main game class that runs everything
+class Game
   def initialize
-    @pegs = Pegs.new
-    @keys = Keys.new
     puts MastermindRules.new.display_rules
   end
 
-  def computer_gen_code
-    @comp_code = [rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
+  def choose_game_type
+    puts 'Choose 1 to play as the codebreaker and 2 to play as the codemaker'
+    @option = gets.to_i
   end
 
-  def player_choice
-    puts 'Enter a 4 digit code to guess the secret code'
-    @choice = gets.split('').to_a
-    @choice.pop # removes carriage return rom array
-    @choice.map!(&:to_i)
-  end
-
-  def choice_valid?
-    true if @choice.all?(1..6)
-  end
-
-  def convert_choice_to_pegs
-    @convert = @choice.map { |number| number - 1 }
-  end
-
-  def display_code
-    puts ' '
-    puts "#{@pegs.pegs[@convert[0]]} #{@pegs.pegs[@convert[1]]} #{@pegs.pegs[@convert[2]]} #{@pegs.pegs[@convert[3]]}"
-    puts
-  end
-
-  def move
-    over?
-    @turn = 1
-    player_turn while @turn < 13
-  end
-
-  def player_turn
-    player_choice
-    if choice_valid? == true
-      convert_choice_to_pegs
-      display_code
-      @turn += 1
+  def initialize_game_type
+    if @option == 1
+        HumanGame.new
+    elsif @option == 2
+        CompGame.new
     else
-      puts 'Please choose a valid 4 digit code'
+      puts 'Please choose a valid option'
     end
   end
 end
-
-game = HumanGame.new
-game.player_turn
-
-
