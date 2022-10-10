2265b499bcdde5cced2a070ecf48cb7fbcbf1765
missopi <missopi@me.com>
2022-10-10 15:39:25 +0100

    Add game end method that displays comp code

--- human_game.rb
+++ human_game.rb
@@ -57,6 +57,27 @@ class HumanGame
     true if player_choice == @comp_code
   end
 
+  def convert_comp_code
+    @comp_ct = @comp_code.map { |number| number - 1 }
+  end
+
+  def display_comp_code
+    convert_comp_code
+    puts "#{@pegs.pegs[@comp_ct[0]]} #{@pegs.pegs[@comp_ct[1]]} #{@pegs.pegs[@comp_ct[2]]} #{@pegs.pegs[@comp_ct[3]]}"
+  end
+
+  def game_end
+    if win? == true
+      @turn = 13
+      puts 'Congratulations, you guessed the code correctly'
+      puts ' '
+    elsif lose == true
+      puts "Unfortunately you didn't guess the code correctly"
+      puts ' '
+    end
+    display_comp_code
+  end
+
   def lose?
     true if player_choice != @comp_code && @turn == 12
   end
