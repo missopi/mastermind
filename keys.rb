041cebf46637f5cd86249134b67ccdf63c1e65ba
missopi <missopi@me.com>
2022-10-13 22:00:57 +0100

    Condense code for blank lines

--- keys.rb
+++ keys.rb
@@ -5,14 +5,12 @@ require 'colorize'
 # handles the coloured keys that show when guesses are correct
 class Keys
   attr_reader :keys
-  
+
   def initialize
     @keys = [' ● '.colorize(:color => :white), ' ● '.colorize(:color => :red)]
   end
-  
+
   def initial_keys
-    puts ' '
-    puts "#{keys[1]}#{keys[1]}#{keys[0]}#{keys[0]}"
-    puts ' '
+    puts "\n#{keys[1]}#{keys[1]}#{keys[0]}#{keys[0]}\n"
   end
 end
