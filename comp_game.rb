c34e41095f6bf76cd46ffd17b53724a047d27c6e
missopi <missopi@me.com>
2022-10-16 20:21:25 +0100

    Add computer_turn method and change names
    Free up computer_guess method to be able to do more than randomly guess

--- comp_game.rb
+++ comp_game.rb
@@ -11,22 +11,30 @@ class CompGame
   def move
     over?
     @guess = 1
-    display_guess while @guess < 13
+    computer_turn while @guess < 13
   end
 
   def user_input_valid?
     true if @user_input.all?(1..6)
   end
 
-  def computer_guess
-    # comp choice is currently random - will need to change so has a strategy later
+  def computer_first_guess
     @comp_random_code = [rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
+  end
+
+  def convert_guess_to_index
     @index = @comp_random_code.map { |number| number - 1 }
   end
 
   def display_guess
-    computer_guess
-    puts "\nThe computer chose: #{@peg.colour[@index[0]]} #{@peg.colour[@index[1]]} #{@peg.colour[@index[2]]} #{@peg.colour[@index[3]]}\n"
+    convert_guess_to_index
+    print "\nThe computer chose: "
+    print "#{@peg.colour[@index[0]]} #{@peg.colour[@index[1]]} #{@peg.colour[@index[2]]} #{@peg.colour[@index[3]]}\n"
+  end
+
+  def computer_turn
+    computer_first_guess
+    display_guess
     @guess += 1
     win
   end
