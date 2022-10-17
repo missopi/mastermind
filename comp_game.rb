7db0f5a1e73e96e5559a3a2a5d6b26de16192107
missopi <missopi@me.com>
2022-10-17 20:54:49 +0100

    Change variable names so easier to read

--- comp_game.rb
+++ comp_game.rb
@@ -6,36 +6,34 @@ require 'colorize'
 class CompGame
   def initialize
     @peg = Pegs.new
-    @code = Array.new(4) { rand(1..6) }
-    @code = [2, 5, 5, 3]
+    puts 'Choose a 4 digit code for the computer to guess.'
+    @user_input = gets.chomp.split('').map!(&:to_i)
     @total_guesses = 12
-
-    print 'Randomly generated start code: '
-    display @code
-    puts "\n"
+    display @user_input
+    puts
   end
 
   def play
-    num = 1
-    nums = []
-    guess = [num] * 4
+    number = 1
+    numbers = []
+    guess = [number] * 4
     @total_guesses.times do
-      print "\nComputer guessed: "
+      print "\nThe computer guessed: "
       display guess
-      puts "\n"
+      puts
 
-      if guess == @code
-        puts 'The computer guessed the code correctly.'
+      if guess == @user_input
+        puts "\nThe computer guessed the code correctly."
         return
       end
 
-      red_pins, white_pins = check guess
-      ((red_pins + white_pins) - nums.length).times { nums << num }
+      red_keys, white_keys = check guess
+      ((red_keys + white_keys) - numbers.length).times { numbers << number }
 
-      guess = nums.clone
+      guess = numbers.clone
       if guess.length < 4
-        num += 1
-        (4 - nums.length).times { guess << num }
+        number += 1
+        (4 - numbers.length).times { guess << number }
       else
         guess.shuffle!
       end
@@ -51,12 +49,12 @@ class CompGame
   def check(guess)
     h_matches = []
     c_matches = []
-    red_pins = 0
-    white_pins = 0
+    red_keys = 0
+    white_keys = 0
 
     guess.each_index do |i|
-      if guess[i] == @code[i]
-        red_pins += 1
+      if guess[i] == @user_input[i]
+        red_keys += 1
         h_matches << i
         c_matches << i
       end
@@ -65,17 +63,17 @@ class CompGame
     guess.each_index do |i|
       next if h_matches.include?(i)
 
-      @code.each_index do |j|
+      @user_input.each_index do |j|
         next if c_matches.include?(j)
 
-        if guess[i] == @code[j]
-          white_pins += 1
+        if guess[i] == @user_input[j]
+          white_keys += 1
           h_matches << i
           c_matches << j
         end
       end
     end
 
-    [red_pins, white_pins]
+    [red_keys, white_keys]
   end
 end
